2fa252a7c464d134ed78dd2af1773a1f47758588
emrhnayz <emirhan.ayaz171@gmail.com>
2024-02-28 23:08:23 +0100

    v1

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import React, { useState } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 import { auth } from '../auth/firebase';
 import { toastErrorNotify, toastSuccessNotify } from '../helpers/ToastNotify';
 import { useNavigate } from 'react-router-dom';
@@ -16,6 +16,11 @@ const AuthContextProvider = ({ children }) => {
     let navigate = useNavigate();
 
     const [currentUser, setCurrentUser] = useState()
+
+    useEffect(()=>{
+        userObserver()
+    },[])
+    console.log(currentUser);
 
     const createUser = async (email, password) => {
 
@@ -54,17 +59,19 @@ const AuthContextProvider = ({ children }) => {
         signOut(auth)
         toastSuccessNotify("Logged out successfully!")
     }
+    const userObserver = () => {
+        //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const { email, displayName, photoURL } = user;
+                setCurrentUser({ email, displayName, photoURL });
+            } else {
+                setCurrentUser(false);
+            }
+        });
+    };
 
-    const userObserver = (auth, user) => {
-        if (user) {
-            const { email, displayName, photoURL, } = user
-            setCurrentUser(email,displayName,photoURL)
-        } else {
-            setCurrentUser(false)
-        }
-    }
-
-    const values = { createUser, signIn, logOut,userObserver }
+    const values = { createUser, signIn, logOut, userObserver }
     return (
         <AuthContex.Provider value={values}>
             {children}
