43c41b39b94246beff0eb3541d2606cf56c14755
emrhnayz <emirhan.ayaz171@gmail.com>
2024-03-01 22:52:33 +0100

    v3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext,";
 
 
 const App = () => {
   return (
     <div>
       <AuthContextProvider>
-        <AppRouter/>
-        <ToastContainer/>
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
       </AuthContextProvider>
 
     </div>
--- src/context/MovieContext,.js
+++ src/context/MovieContext,.js
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import axios from 'axios';
 
 export const MovieContext = createContext();
